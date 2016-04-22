548025d349c322dd38a316eb3869adaa0d83b18f
Matt de Marillac <mattdemarillac@Matts-Air.gateway>
2016-04-22 19:25:06 +1000

    finished blog CRUD

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+//var paginate = require('express-paginate');
+
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('data/blogdb');
 
@@ -11,7 +13,6 @@ db.serialize(function() {
 	'`text`	text,'+
 	'`type` text)');
     db.run("INSERT INTO blog (title, text, type) VALUES (?, ?, ?)", "Home Page", "Welcome to my blog, please feel free to browse the site!", "page");
-     db.run("INSERT INTO blog (title, text, type) VALUES (?, ?, ?)", "Hello World", "Hiii", "page");
 });
 
 /* GET home page. */
@@ -20,21 +21,27 @@ router.get('/', function(req, res, next) {
     db.all("SELECT * FROM blog;", function(err2, result){
     console.log(result);
    
-   
-   res.render('index', { title: 'Hello', body: 'hello wold', blogs: result });
+    res.render('index', { title: 'Hello', body: 'hello wold', blogs: result });
    });
 });
 
 router.get('/id/:id', function(req, res, next) {
-   res.render('about', { title: 'View Blog', id: req.param('id')});
+	db.get("SELECT * FROM blog where id = " + req.param('id'), function(err, result){
+   res.render('about', {id: req.param('id'), title: result.title, text: result.text});
+  });
+});
 
+
+router.post('/add', function(req, res, next) {
+  db.run("INSERT INTO blog (title, text, type) VALUES (?, ?, ?)", req.body.title, req.body.text, "blog");
+  res.redirect('/');
 });
 
 router.post('/', function(req, res, next) {
  db.run("UPDATE blog SET title = '"+req.body.title+"', text = '"+req.body.text+"' WHERE id = 1;", function(err, row){
         if (err){
             res.status(500);
-            res.render('index', { title: err });
+            res.render('error', { title: err });
         }
         else {
             res.status(202);
@@ -42,6 +49,17 @@ router.post('/', function(req, res, next) {
         }
         res.end();
     });
+});
+
+router.post('/id/:id/delete', function(req, res, next) {
+	console.log(req.param('id'));
+  db.run("DELETE From blog where id = " + req.param('id'));
+  res.redirect('/');
+});
+
+router.post('/id/:id/update', function(req, res, next) {
+  db.run("UPDATE blog set title = '"+req.body.title+"', text = '"+req.body.text+"' where id = " + req.param('id'));
+  res.redirect('/id/'+req.param('id'));
 });
 
 module.exports = router;
